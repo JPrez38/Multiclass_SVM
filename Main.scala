1bf1f6fb32d615af5c01b2bef69a5bb56b454199
Jacob Preston <jacobpreston4@gmail.com>
2013-10-06 17:24:42 -0400

    made feature vectors

--- Main.scala
+++ Main.scala
@@ -3,13 +3,24 @@ package multi_svm
 import math._
 
 object Main {
+	val trainingData = getDataFromFile("data/mnist_train.txt")
+	val testData = getDataFromFile("data/mnist_test.txt")
 	def main(args: Array[String]) = {
 		val start = System.currentTimeMillis
+		runMultiClassSVM()
+
 		val end = System.currentTimeMillis
 		println("Total Running Time of all Tests: " + (end-start)/1000.0 + " seconds")
 	}
 
 	def runMultiClassSVM() = {
-		
+		val featureVectors = Setup.featureGen(trainingData)
+	}
+
+	def getDataFromFile(file: String) : String = {
+		import scala.io.Source 
+		val source = Source.fromFile(file)
+		val lines = source.getLines mkString "\n"
+		return lines
 	}
 }
--- Setup.scala
+++ Setup.scala
@@ -3,7 +3,31 @@ package multi_svm
 import math._
 
 object Setup {
-	def featureGen() = {
+	def featureGen(inputFile: String) : Array[(Int,Array[Double])] = {
+		val digits = inputFile.split("\n")
+		var featureVectors = new Array[(Int,Array[Double])](digits.length)
+		var i = 0
+		for (digit <- digits) {
+			val temp = digit.split(",")
+			val numberedTemp = new Array[Int](temp.length)
+			for (i <- 0 to temp.length-1) {
+				numberedTemp(i) = temp(i).toInt
+			}
+			val tmp = numberedTemp.splitAt(1)
+			val actualDigit = tmp._1
+			var featureVector = new Array[Double](tmp._2.length)
+			for (i <- 0 to tmp._2.length-1) {
+				featureVector(i) = 2*(tmp._2(i)/255.0)-1
+			}
+
+			featureVectors(i) = (actualDigit.head,featureVector)
+			i+=1
+		}
+
+		//println(featureVectors(10)._1)
+		//featureVectors(10)._2.foreach(x => print(f"$x%1.2f ,"))
 		
+
+		return featureVectors
 	}
 }
