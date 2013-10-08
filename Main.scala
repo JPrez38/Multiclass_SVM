0323b428638a63103ec4cc7a0fdaaab4bee02a01
Jacob Preston <jacobpreston4@gmail.com>
2013-10-08 01:13:44 -0400

    Done

--- Main.scala
+++ Main.scala
@@ -8,15 +8,18 @@ object Main {
 	def main(args: Array[String]) = {
 		val start = System.currentTimeMillis
 		//runMultiClassSVM(true,2000,pow(2,-3))
-		//runCrossValidationSVM(false,2000,pow(2,-3))
-		val featureVectors = Setup.featureGen(trainingData)
-		outputScaledData(featureVectors)
+		var lambdas = List[Double]()
+		for (i <- -5 to 1) {lambdas::= (math.pow(2,i))}
+		lambdas = lambdas.reverse
+		//lambdas.foreach(x => runCrossValidationSVM(false,2000,x))
+		runMultiClassSVM(true,2000,math.pow(2,-3))
 
 		val end = System.currentTimeMillis
-		//println("Total Running Time of all Tests: " + (end-start)/1000.0 + " seconds")
+		println("Total Running Time of all Tests: " + (end-start)/1000.0 + " seconds")
 	}
 
 	def runMultiClassSVM(useTestData: Boolean,trainingSize:Int,lambda: Double) = {
+		val start = System.currentTimeMillis
 		val featureVectors = Setup.featureGen(trainingData)
 		val tmp = featureVectors.splitAt(trainingSize) /*splits training data set for validation set*/
 		val trainingDataSet = tmp._1
@@ -24,15 +27,20 @@ object Main {
 		val svm_classifiers = MultiSVM.multi_svm_train(trainingDataSet,lambda)
 		val testError = MultiSVM.multi_svm_test(validationDataSet,svm_classifiers)
 		println(f"Test Error: $testError%1.3f for lambda of $lambda")
+		val end = System.currentTimeMillis
+		println("Total Running Time of algorithm: " + (end-start)/1000.0 + " seconds")
 	}
 
 	def runCrossValidationSVM(useTestData: Boolean,trainingSize:Int,lambda: Double) = {
+		val start = System.currentTimeMillis
 		val featureVectors = Setup.featureGen(trainingData)
 		val tmp = featureVectors.splitAt(trainingSize) /*splits training data set for validation set*/
 		val trainingDataSet = tmp._1
 		val validationDataSet = if(useTestData) Setup.featureGen(testData) else tmp._2
 		val testError = MultiSVM.cross_validation_svm(trainingDataSet,lambda,5)
-		println(f"Test Error: $testError%1.3f for lambda of $lambda")
+		println(f"Cross Validation Error: $testError%1.3f for lambda of $lambda")
+		val end = System.currentTimeMillis
+		println("Total Running Time of algorithm: " + (end-start)/1000.0 + " seconds")
 	}	
 
 	def getDataFromFile(file: String) : String = {
